cfd6aec4b75efb5176e32460b469a58f66b7e20a
ziggi <xziggix@gmail.com>
2015-04-12 18:54:33 +0300

    jQuery обновлён до 2.1.3

--- module/jquery/controller.php
+++ module/jquery/controller.php
@@ -3,6 +3,6 @@
 class Controller_jQuery extends Controller {
 
 	public function includeScript() {
-		$this->view->addScript('jquery-2.1.1.min.js', 'jquery');
+		$this->view->addScript('jquery-2.1.3.min.js', 'jquery');
 	}
 }
