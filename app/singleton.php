dad2766aa80412b056b6f0235268ae2d593641db
Sergey Marochkin <xziggix@gmail.com>
2015-10-30 12:57:59 +0300

    Добавлен абстрактный класс Singleton

---
+++ app/singleton.php
@@ -0,0 +1,27 @@
+<?php
+
+abstract class Singleton
+{
+	protected function __construct()
+	{
+
+	}
+
+	final private function __clone()
+	{
+
+	}
+
+	final public static function getInstance()
+	{
+		static $instances = array();
+
+		$called_class = get_called_class();
+
+		if (!isset($instances[$called_class])) {
+			$instances[$called_class] = new $called_class();
+		}
+
+		return $instances[$called_class];
+	}
+}
